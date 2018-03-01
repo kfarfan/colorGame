57b954d77989fbd81655e61f9b64be6566773345
kfarfan <34223564+kfarfan@users.noreply.github.com>
2018-02-28 20:52:38 -0500

    added hard-mode button

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector('h1');
+var easyMode = document.getElementById('easyMode');
+var hardMode = document.getElementById('hardMode');
 
 colorDisplay.textContent = pickedColor;
 
@@ -30,6 +32,30 @@ for(var i = 0; i < squares.length; i++){
   })
 }
 
+hardMode.addEventListener('click', function(){
+  h1.style.backgroundColor = '#232323';
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+
+    squares[i].addEventListener('click', function(){
+      var clickedColor = this.style.backgroundColor;
+      if(clickedColor === pickedCOlor){
+        messageDisplay.textContent = "correct!";
+        changeColors(clickedColor);
+        h1.style.backgroundColor = clickedColor;
+      } else {
+      this.style.backgroundColor = '#232323';
+      messageDisplay.textContent = "Try Again";
+      }
+    })
+  }
+
+});
+
+
 function changeColors(color){
   //loop through all squares
   for(var i = 0; i < squares.length; i++){
@@ -39,7 +65,7 @@ function changeColors(color){
 }
 
 function pickColor(){
-  var random = Math.floor(Math.random() * colors.length + 1)
+  var random = Math.floor(Math.random() * colors.length)
   return colors[random];
 }
 
